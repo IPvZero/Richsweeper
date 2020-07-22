7ac08c2fc77781fcd7f55f41e30d21da02490668
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-07-22 15:17:08 +0100

    Create richsweeper.py

---
+++ richsweeper.py
@@ -0,0 +1,41 @@
+import os
+import time
+from subprocess import Popen, DEVNULL
+from rich.console import Console
+from rich.table import Table
+import itertools
+import ipaddress
+
+clear = "clear"
+os.system(clear)
+localtime = time.asctime(time.localtime(time.time()))
+active_list = []
+inactive_list = []
+p = {}
+
+subnet = input("Please enter the network: ")
+network = ipaddress.ip_network(subnet)
+
+for n in network.hosts(): # start ping processes
+    ip = str(n)
+    p[ip] = Popen(['ping', '-c', '4', '-i', '0.2', ip], stdout=DEVNULL)
+
+while p:
+    for ip, proc in p.items():
+        if proc.poll() is not None:
+            del p[ip]
+            if proc.returncode == 0:
+                active_list.append(ip)
+            elif proc.returncode == 1:
+                inactive_list.append(ip)
+            else:
+                print(f"{ip} ERROR")
+            break
+
+table = Table(title="PING REPORT \n" + localtime)
+table.add_column("Active Hosts", justify="center", style="green")
+table.add_column("Inactive Hosts", justify="center",style="red")
+for (a,i) in itertools.zip_longest(active_list,inactive_list):
+    table.add_row(a,i)
+console = Console()
+console.print(table)
